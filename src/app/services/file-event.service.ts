c82334c6e297f650ccc848fce3e90084b34c1c08
Tony Nguyen <tnguyen@cclgroup.com>
2024-09-12 12:41:02 -0700

    change env global

--- src/app/services/file-event.service.ts
+++ src/app/services/file-event.service.ts
@@ -2,14 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { FileEvent } from '../model/file-event.model'
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FileEventService {
-  private apiUrl = 'https://localhost:7068/api/FileEvent/ByReferenceId'; // Api end point
-
-  mockData: FileEvent[] = []; //
+  private apiUrl = `${environment.apiUrl}/FileEvent/ByReferenceId`; // api end point
 
   constructor(private http: HttpClient) { 
     

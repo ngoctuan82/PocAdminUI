3196e4ae0dadd7d996cfd67b60f16c87e5fbd0fc
Tony Nguyen <tony.nguyen@pelesys.com>
2024-09-11 23:14:45 -0700

    env change

--- src/app/services/invalid-data.service.ts
+++ src/app/services/invalid-data.service.ts
@@ -2,31 +2,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { delay, Observable, of } from 'rxjs';
 import { InvalidData } from '../model/invalid-data.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InvalidDataService {
-  private apiUrl = 'https://localhost:7068/api/InvalidData'; // api end point
+  private apiUrl = `${environment.apiUrl}/InvalidData`; // api end point
 
   constructor(private http: HttpClient) { }
 
   getAll(): Observable<InvalidData[]> {
     return this.http.get<InvalidData[]>(this.apiUrl);
-    // mock wait for emulate the response from api
-    // return of([
-    //   { id: 1, name: 'Model A 1', effectiveDate: new Date(), value: 100, referenceId: 101, validated:false },
-    //   { id: 2, name: 'Model A 2', effectiveDate: new Date(), value: 200, referenceId: 102, validated:false },
-    //   { id: 3, name: 'Model A 3', effectiveDate: new Date(), value: 300, referenceId: 103, validated:false },
-    //   // Add more mock data as needed
-    // ]).pipe(delay(1000));
   }
 
   getById(id: number): Observable<InvalidData> {
-    return this.http.get<InvalidData>(`${this.apiUrl}/${id}`);
-    // return of(
-    //   { id: 1, name: 'Model A 1', effectiveDate: new Date(), value: 100, referenceId: 101, validated:false }
-    // )
+    return this.http.get<InvalidData>(`${this.apiUrl}/${id}`);    
   }
   
 }
